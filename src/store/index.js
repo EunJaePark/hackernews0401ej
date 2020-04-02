0274178845f7f6afe081beae457e1db9a9edf52c
EunJaePark <53172233+EunJaePark@users.noreply.github.com>
2020-04-02 16:47:19 +0900

    store
    - 데이터 불러오는 것을 store 이용해서 불러오는 방식으로 변경.

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { fetchListItem } from '../api/index.js'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+export const store = new Vuex.Store({
   state: {
+    list: []
   },
   mutations: {
+    SET_LIST(state, list) {  //mutations에서는 위의 state를 자동으로 불러옴. ()속에 적은 state가 위의 state를 불러온것으로, 이름은 마음대로 바꿛 된다.
+      state.list = list
+    }
   },
-  actions: {
+  actions: {  //actions의 주요 역할은 mutations를 실행시키는 것. mutations가 실행되면 당연히 state도 변경되겠지!
+    FETCH_LIST(context, pageNum) {  //NewsView.vue에서 dispatch로 받아온 'news'라는 데이터를 pageNum에 넣은것.
+      return fetchListItem(pageNum) 
+      .then(res => {
+        context.commit('SET_LIST', res.data)
+        return res
+      })
+      .catch(err => {
+        console.log(err);
+        
+      })
+    }
   },
   modules: {
   }
